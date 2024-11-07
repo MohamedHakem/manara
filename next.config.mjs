d043f15730c2ab32b9c3b6281557834d521cc90b
MohamedHakem <mohamedhakem959628@gmail.com>
2024-11-08 00:42:05 +0200

    chore: remove unused remote image patterns and set minimum cache TTL for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '**'
-      }
-    ]
+    minimumCacheTTL: 31536000
   }
 };
 
