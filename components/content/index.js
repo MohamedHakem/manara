e4fcbf5a6ad3ceded3977a0ff05321f859ccaf6d
MohamedHakem <mohamedhakem959628@gmail.com>
2024-11-18 17:54:21 +0200

    refactor: remove deprecated content components and clean up related files

--- components/content/index.js
+++
@@ -1,5 +0,0 @@
-export { default as PageContentLearn } from './page-content-learn';
-export { default as PageContentTrack } from './page-content-track';
-export { default as PageContentCourse } from './page-content-course';
-export { default as PageContentLesson } from './page-content-lesson';
-export { default as PageContentModule } from './page-content-module';
