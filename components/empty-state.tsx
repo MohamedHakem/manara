548f4e7ecfc488fb6c91be4f34d4aa63d6986955
MohamedHakem <mohamedhakem959628@gmail.com>
2024-11-20 11:28:37 +0200

    fix: adjust layout and spacing in Empty component for better visual consistency

--- components/empty-state.tsx
+++ components/empty-state.tsx
@@ -1,6 +1,6 @@
 export default function Empty() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4 md:p-12 md:border md:rounded-xl">
+    <div className="flex flex-col items-center justify-center gap-1 md:gap-2 p-4 md:p-12 h-40 md:h-56 md:border md:rounded-xl">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -11,7 +11,7 @@ export default function Empty() {
         strokeWidth="1"
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="lucide lucide-animal animal-icon w-40 h-40 text-black/80 mt-20"
+        className="lucide lucide-animal animal-icon w-40 text-black/80 flex-1"
       >
         <path d="M13 16a3 3 0 0 1 2.24 5"></path>
         <path d="M18 12h.01"></path>
@@ -19,9 +19,8 @@ export default function Empty() {
         <path d="M20 8.54V4a2 2 0 1 0-4 0v3"></path>
         <path d="M7.612 12.524a3 3 0 1 0-1.6 4.3"></path>
       </svg>
-      <h2 className="prose md:prose-2xl">
-        {' '}
-        No Content here yet! I&apos;m working on it{' '}
+      <h2 className="prose md:prose-2xl h-7">
+        No Content here yet! I&apos;m working on it
       </h2>
       {/* <p className="prose">
         Continue learning, practice, or connect with your peers in the Manara Community!  
